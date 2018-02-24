e1a2febec201fe8c730b63d239d7b0a4e4202fa9
Rishabh <asthana4@illinois.edu>
2018-02-24 17:14:29 -0600

    api_integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Progress from './components/progress/progress';
 import Coin from './components/coin/coin';
+import axios from 'axios';
 
 
 class App extends Component {
@@ -10,6 +11,18 @@ class App extends Component {
     this.state = {
       machine_prices: [2,4,9,10,11,24,56,89,100,120]
     }
+  }
+  componentDidMount(){
+      var self = this;
+      setInterval(function(){
+          axios.get("http://localhost:8080/")
+          .then(function(response){
+           let data = response.data;
+           console.log(data);
+           self.setState({btc: data.btc, eth: data.eth, xrp: data.xrp, timestamp: data.timestamp});
+          });
+      },3000);
+      
   }
   render() {
     return (
